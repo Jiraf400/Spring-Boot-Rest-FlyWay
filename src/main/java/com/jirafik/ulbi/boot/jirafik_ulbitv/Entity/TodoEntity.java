65294eba2c2fbcfdb65eeb1dd4e1466c791883d2
jiraf400 <sparolem234@gmail.com>
2023-01-26 05:52:24 +0300

    added lombok

--- src/main/java/com/jirafik/ulbi/boot/jirafik_ulbitv/Entity/TodoEntity.java
+++ src/main/java/com/jirafik/ulbi/boot/jirafik_ulbitv/Entity/TodoEntity.java
@@ -1,9 +1,15 @@
 package com.jirafik.ulbi.boot.jirafik_ulbitv.Entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "todo_entity")
+@NoArgsConstructor
+@Getter
+@Setter
 public class TodoEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,45 +22,4 @@ public class TodoEntity {
     @JoinColumn(name = "user_id")
     private UserEntity user;
 
-    public TodoEntity(){}
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Boolean getCompleted() {
-        return completed;
-    }
-
-    public void setCompleted(Boolean completed) {
-        this.completed = completed;
-    }
-
-    public UserEntity getUser() {
-        return user;
-    }
-
-    public void setUser(UserEntity user) {
-        this.user = user;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
--- src/main/java/com/jirafik/ulbi/boot/jirafik_ulbitv/Entity/UserEntity.java
+++ src/main/java/com/jirafik/ulbi/boot/jirafik_ulbitv/Entity/UserEntity.java
@@ -1,13 +1,18 @@
 package com.jirafik.ulbi.boot.jirafik_ulbitv.Entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @Entity
 @Table(name = "user_entity")
+@NoArgsConstructor
+@Getter
+@Setter
 public class UserEntity {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -17,43 +22,41 @@ public class UserEntity {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
     private List<TodoEntity> todos;
 
-    public UserEntity() {}
-
-//    public userEntity(Long id, String username, String password) {
+    //    public userEntity(Long id, String username, String password) {
 //        this.id = id;
 //        this.username = username;
 //        this.password = password;
 //    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public List<TodoEntity> getTodos() {
-        return todos;
-    }
-
-    public void setTodos(List<TodoEntity> todos) {
-        this.todos = todos;
-    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public List<TodoEntity> getTodos() {
+//        return todos;
+//    }
+//
+//    public void setTodos(List<TodoEntity> todos) {
+//        this.todos = todos;
+//    }
 }
